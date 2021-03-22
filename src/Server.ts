bca7abde104ece00d9784dcda8f70198c24380ed
happybearday <45377518+happybearday@users.noreply.github.com>
2021-03-22 23:05:34 +0100

    Add dotenv parser & rename directory shared to helpers

--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
-import Logger from "./shared/Logger";
+import Logger from "./helpers/Logger";
 
 const app = express();
 app.use(express.json());
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import app from './Server';
+import app from "./Server";
+import { APP_PORT } from "./helpers/params";
 // Start the server
-const port = Number(process.env.PORT || 3000);
+const port = Number(APP_PORT || 3000);
 app.listen(port, () => {
-   //logger.info('Express server started on port: ' + port);
-   console.log('Express server started on port: http://localhost:' + port);
+  //logger.info('Express server started on port: ' + port);
+  console.log("Express server started on port: http://localhost:" + port);
 });
